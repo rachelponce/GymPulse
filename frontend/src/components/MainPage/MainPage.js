be785707b028698ff1ce0e4a7ff15bc0aa486bb2
Lorenz Carvajal <lorenzcarvajal@ufl.edu>
2024-02-10 16:29:45 -0500

    Added button to main page, works on #1

--- frontend/src/components/MainPage/MainPage.js
+++ frontend/src/components/MainPage/MainPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { MainPageWrapper } from './MainPage.styled';
+import Button from '@mui/material/Button';
 
 const MainPage = () => (
  <MainPageWrapper>
     MainPage Component
+   <Button>Click Me!</Button>
  </MainPageWrapper>
 );
 
--- frontend/src/components/MainPage/MainPage.styled.js
+++ frontend/src/components/MainPage/MainPage.styled.js
@@ -1,7 +1,4 @@
 import styled from 'styled-components';
 
 export const MainPageWrapper = styled.div`
-    background: blue;
-    width: 50vw;
-    height: 50vh;
 `;
