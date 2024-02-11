88496a162a770afc88c352de9f12a796c5a85b88
Lorenz Carvajal <lorenzcarvajal@ufl.edu>
2024-02-10 19:04:11 -0500

    Click handle progress for #2

--- frontend/src/components/GymTimePage/GymTimePage.js
+++ frontend/src/components/GymTimePage/GymTimePage.js
@@ -2,29 +2,36 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { GymTimePageWrapper, Card, Photo, Title, CardsWrapper, Header } from './GymTimePage.styled';
 
-const GymTimePage = () => (
- <GymTimePageWrapper className='wrapper'>
-   <Title>Choose your GymPulse</Title>
-   <CardsWrapper>
-      <Card>
-         <Photo src="cardio-pic.jpg" alt="cardio pic" />
-         <Header>Cardio</Header>
-      </Card>
-      <Card>
-         <Photo src="back-pic.jpg" alt="back pic" />
-         <Header>Back and Bicep</Header>
-      </Card>
-      <Card>
-         <Photo src="tricep-pic.jpg" alt="tricep pic" />
-         <Header>Chest and Tricep</Header>
-      </Card>
-      <Card>
-         <Photo src="leg-pic.jpg" alt="leg pic" />
-         <Header>Legs</Header>
-      </Card>
-   </CardsWrapper>
- </GymTimePageWrapper>
-);
+const GymTimePage = () => {
+
+   const handleClick = async () => {
+      
+   }
+
+   return (
+      <GymTimePageWrapper className='wrapper'>
+         <Title>Choose your GymPulse</Title>
+         <CardsWrapper>
+            <Card onClick={handleClick()}>
+               <Photo src="cardio-pic.jpg" alt="cardio pic" />
+               <Header>Cardio</Header>
+            </Card>
+            <Card>
+               <Photo src="back-pic.jpg" alt="back pic" />
+               <Header>Back and Bicep</Header>
+            </Card>
+            <Card>
+               <Photo src="tricep-pic.jpg" alt="tricep pic" />
+               <Header>Chest and Tricep</Header>
+            </Card>
+            <Card>
+               <Photo src="leg-pic.jpg" alt="leg pic" />
+               <Header>Legs</Header>
+            </Card>
+         </CardsWrapper>
+      </GymTimePageWrapper>
+   );
+};
 
 GymTimePage.propTypes = {};
 
